8006e76745ed3047f56c9e89a29fbb2bb2dd0193
Borzenko Anton <borzenko_a@bk.ru>
2017-09-14 00:20:48 +0300

    Added other importers

---
+++ contracts/Importer5.sol
@@ -0,0 +1,60 @@
+pragma solidity ^0.4.15;
+
+import "./AbstractImporter.sol";
+
+contract Importer5 is AbstractImporter {
+    function _import() internal {
+        ico.importTokens(0x114331b4f2813d7636db06e012f5f6a2385dff80);
+        ico.importTokens(0xca8f1d4b1ef5051698644fc970168c97149b263b);
+        ico.importTokens(0xf7dde0e382c8789641a2dfbc31ea0c15b9a10077);
+        ico.importTokens(0xc29a667c2cb3c1de51a2b6c2936438db27975404);
+        ico.importTokens(0xaa8c03e04b121511858d88be7a1b2f5a2d70f6ac);
+        ico.importTokens(0x0fd4761d51a415190c367c4f542e06f2f3e9e8f6);
+        ico.importTokens(0xef67a6208097869ce4dc69667d6f391a7620c801);
+        ico.importTokens(0x9052af73bed5b9f592f140caf7d17c69fac0cd32);
+        ico.importTokens(0x7ed1e469fcb3ee19c0366d829e291451be638e59);
+        ico.importTokens(0x1e1931f4ed3bef0fc24c1a6b7dc7933711e8739d);
+        ico.importTokens(0xe91064b4c8d1e9aa611cce2eed5ef8484a31f832);
+        ico.importTokens(0x282c1cfb4f29324c7e4d0a3bb89a28ddfeddfd45);
+        ico.importTokens(0x4c22ce036879bea829955676137b1e2dec8c57ba);
+        ico.importTokens(0x78805795dc090222db3f7b00f1c1325eec40a44d);
+        ico.importTokens(0x1a62a9d02e0556188bcdbc14c4d1320361dbf6de);
+        ico.importTokens(0x77483497fe1972cb07792ade636984a81826efd8);
+        ico.importTokens(0x4c462a2353537143fdd800ed811092a67722d641);
+        ico.importTokens(0x443be6c82a74691b0b3f8f3c6575d2d80602ea8e);
+        ico.importTokens(0x9a14a2c5d0206a5b15a998a657ecf7ebdb05b8cc);
+        ico.importTokens(0x1cecb887f964e000d5950fdfc44ec4d91e288b41);
+        ico.importTokens(0x5bb7fc3b16af14e4b1e37a9fad4df7eb7fadb992);
+        ico.importTokens(0xd908f80aba091f8eb3135e7876d51b5b1a7bb188);
+        ico.importTokens(0xb9d38ad5cc92a97b2dad986ef866b74c56d8fbc4);
+        ico.importTokens(0xcae91b10df2fdced66a942d4e52af9da368245c3);
+        ico.importTokens(0x568ad255195be51e7c0aee6f5e800e3fd009e7f7);
+        ico.importTokens(0x317066a7d0e3e429acb39b143e78af8152890f6b);
+        ico.importTokens(0xe98318528747a12864e046eb8433f915dd0970f7);
+        ico.importTokens(0x22a0e9afb550d1be05d31bf4f1ee2545a37ec9f4);
+        ico.importTokens(0xe6472fffa8d49edd5b7e0005970b15131d939846);
+        ico.importTokens(0xeeafc52c868811e21e8b955fabf7fd60a4ba3822);
+        ico.importTokens(0x0cf1782053040933187ecd39b7c3f2a5eb63a2ca);
+        ico.importTokens(0xf13278c5de06db7562c3c9e72cf3327cf1e31081);
+        ico.importTokens(0xd3a0544f3a23a78d31c8ede32d34ef4152e77f0a);
+        ico.importTokens(0x7f010ffaeda601a9a42517b09130aad754b65e82);
+        ico.importTokens(0x0835533b06d9b313a564c9c025d4517c8abf900d);
+        ico.importTokens(0x054f77801974c287351c680c926f0928904c2019);
+        ico.importTokens(0x3958bd057afa1d1c5aa986cd6b22f290af9153b8);
+        ico.importTokens(0x5653d53d3a8f237a99fd2aa27b2b39dda403a548);
+        ico.importTokens(0x55c82751b2b15854db61e37cde6db269e51ab6f1);
+        ico.importTokens(0x3885ce6cdd20a3c5295fd288173fa84c9436be4c);
+        ico.importTokens(0xbaa3b46fbac6d3382a1e4fffaa85f4cc8932fc35);
+        ico.importTokens(0x22a04f6b2433fd164adf5c7a0c1d4750d9af5a0c);
+        ico.importTokens(0xd3b50894342cf27176b2ee4e4bff938314f6c132);
+        ico.importTokens(0xd167ffd468be89f556f80c0eaab244def6e7eda0);
+        ico.importTokens(0x79e95f5b9679ca97da6823864548b223024f56ff);
+        ico.importTokens(0x74ec465eec889e809258569eb6a525a741cd3ec3);
+        ico.importTokens(0x5852eba36d92255b0221c3c350372c90aca65e08);
+        ico.importTokens(0x3e36fdc09d2bb76800223fdb34d4a8a8aa324a91);
+        ico.importTokens(0xa77cb52ef8c4e2b227ea63e2178918fd6eab936c);
+        ico.importTokens(0x2d569b21e7650c752ea6f98ff491099c7bda8202);
+        ico.importTokens(0xdf4b8646be1670f898e67a8651c30b456afd2b60);
+        ico.importTokens(0xfc1bb3da2d32eb4606a1baef4da771750de7a51c);
+    }
+}
